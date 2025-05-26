b039b4767445e84191acdb5cced66aa79eb11953
Subhaga987 <subhaga508@gmail.com>
2025-05-26 18:36:20 +0530

    Add files via upload

---
+++ hexa.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number"))
+print("hexadecimal of" + str(num) + str(hex(num)))
+print("octal of" + str(num) + str(oct(num)))
+print("square root of" + str(num) + str(num**0.5))
+      
