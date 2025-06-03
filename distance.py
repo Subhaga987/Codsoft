b5bd91c2b75626229da2f8e00f48de9c4a8fb50e
Subhaga987 <subhaga508@gmail.com>
2025-06-03 20:27:29 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,7 @@
+x1=int(input("enter a number"))
+x2=int(input("enter a number"))
+y1=int(input("enter a number"))
+y2=int(input("enter a number"))
+distance=((x2-x1)**2+(y2-y1)**2)**0.5
+print("distance=")
+print(distance)
